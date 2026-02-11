b4374d42cdc78f737a1fd9b968ec1925caa613dd
Plus664 <soccer710a@gmail.com>
2026-02-11 14:50:52 +0900

    Add null check and log for device orientation event

--- script.js
+++ script.js
@@ -2,6 +2,8 @@ const audio = document.getElementById("audio");
 const fill = document.getElementById("fill");
 
 window.addEventListener("deviceorientation", e => {
+  console.log(e.gamma)
+  if (e.gamma == null) return;
   let tilt = e.gamma;
   tilt = Math.max(-45, Math.min(45, tilt));
   const volume = (tilt + 45) / 90;
